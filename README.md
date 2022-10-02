1e5926add983e3deb85931f6773b775289b8d147
AJAY RAJPUT <114806048+iprime11@users.noreply.github.com>
2022-10-02 19:24:29 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Mark9EmojiMeter
+# I-mGroot
+This translator will convert english language to the groot language . if you wanna learn Groot language start from now :)
---
+++ app.js
@@ -0,0 +1,27 @@
+var btntranslate = document.querySelector("#button");
+var textinputer  = document.querySelector("#textinput");
+var textoutputer = document.querySelector("#output");
+
+var serverURl = "https://api.funtranslations.com/translate/groot.json"
+
+function gettranslatedurl(input){
+     return serverURl + "?" + "text=" +input
+
+}
+
+function errorHandler(error){
+
+   console.log("error occured" ,error.code)
+  //  alert("wrong")
+}
+
+function clickhandler() {
+    var inputtext = textinputer.value;
+  fetch(gettranslatedurl(inputtext))
+  .then(response=> response.json())
+  .then(json => {
+      var translatedtext = json.contents.translated;
+      textoutputer.innerText= translatedtext;})
+  .catch(errorHandler) 
+};
+btntranslate.addEventListener("click" , clickhandler)
